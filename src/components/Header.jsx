e970a0c4c297464c71489dfac3197aa0734bfa53
Novanya Mendis <novamendis@gmail.com>
2025-09-01 12:59:19 +0530

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,7 +5,7 @@ function Header() {
     <div
       className="min-h-screen min-w-full"
       style={{
-        backgroundImage:"url('/background.jpg'),
+        backgroundImage:"url('/background.jpg')",
         backgroundSize: "cover",
         backgroundPosition: "center",
         backgroundRepeat: "no-repeat",
